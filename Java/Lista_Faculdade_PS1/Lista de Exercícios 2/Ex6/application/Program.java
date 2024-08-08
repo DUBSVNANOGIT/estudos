2f071d39e6c05ac403a2172a0fc7de826fc0ae92
DUBSVNANOGIT <arthur.bic_@hotmail.com>
2024-08-08 12:29:18 -0300

    Ex06 Done

--- Java/Lista_Faculdade_PS1/Lista de Exercícios 2/Ex6/application/Program.java
+++ Java/Lista_Faculdade_PS1/Lista de Exercícios 2/Ex6/application/Program.java
@@ -5,18 +5,11 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.println(1/2 + 2/2-1 + 3/2-2 + 2-2/3 + 2-1/2 + 2);
-
-        double s = 0;
-        s += 1 / n;
-        for (int i = 1; i<n; i++) {
-            s += i / n - (i - 1);
-            for (int j = 1; j < n; j++) {
-                s += n - (j - 1) / j;
-            }
+        double n = sc.nextDouble();
+        double s = 0.0;
+        for (int i = 1; i<=n; i++) {
+            s += i / (n - (i - 1.0));
         }
-        s += n;
         System.out.println(s);
 
     }
