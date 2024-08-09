3750cd6750983de2be4cee6b2ccb18738e944df0
DUBSVNANOGIT <arthur.bic_@hotmail.com>
2024-08-09 18:50:40 -0300

    L3 - Ex4

---
+++ Java/Lista_Faculdade_PS1/Lista de Exercícios 3/Ex4/README.md
@@ -0,0 +1,5 @@
+Faça um programa leia um conjunto de inteiros e movimente todas as ocorrências do valor 0 para
+as posições mais a direita do vetor.
+Exemplo:
+
+![img.png](img.png)
---
+++ Java/Lista_Faculdade_PS1/Lista de Exercícios 3/Ex4/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Quantidade de elementos do conjunto: ");
+        int n = sc.nextInt();
+        int[] vet = new int[n];
+        for(int i =0; i < n; i++) {
+            System.out.printf("Informe o %d° elemento: ", i+1);
+            vet[i] = sc.nextInt();
+        }
+        System.out.println("Dados na ordem original: " + Arrays.toString(vet));
+        for(int i = 0; i < vet.length; i++) {
+            if (vet[i] == 0 && vet[i + 1] != 0) {
+                vet[i] = vet[i + 1];
+                vet[i + 1] = 0;
+            }
+        }
+        System.out.println("Dados reorganizados....: " + Arrays.toString(vet));
+
+    }
+}
