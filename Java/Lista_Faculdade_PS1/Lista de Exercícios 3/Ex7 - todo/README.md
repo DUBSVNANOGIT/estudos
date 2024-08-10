326325ea22db4fecad2df70c29839156bc8be48b
DUBSVNANOGIT <arthur.bic_@hotmail.com>
2024-08-10 00:32:12 -0300

    L3 - Ex7

---
+++ Java/Lista_Faculdade_PS1/Lista de Exercícios 3/Ex7 - todo/README.md
@@ -0,0 +1,8 @@
+Escreva um programa em Java que leia do teclado o conteúdo de um array bidimensional (matriz)
+de inteiros com duas linhas e três colunas. Após os valores terem sido carregados, o programa
+deverá apresentar o conteúdo na forma original e na forma transposta.
+Obs: A transposta de uma matriz reorganiza os elementos de modo que as linhas passam a
+configurar-se como colunas.
+Exemplo:
+
+![img.png](img.png)
---
+++ Java/Lista_Faculdade_PS1/Lista de Exercícios 3/Ex7 - todo/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = 2, column = 3;
+        int[][] matriz = new int[row][column];
+        System.out.println("Entre com os elementos da matriz: ");
+        for(int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                System.out.printf("Posição %d, %d: ",i+1,j+1);
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("A matriz original é: ");
+
+        for (int i= 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+               System.out.print(matriz[i][j] + " ");
+            }
+        }
+    }
+}
