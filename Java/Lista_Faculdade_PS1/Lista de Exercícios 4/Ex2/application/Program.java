54c016f61bcd4311601806d63f428437935dd26b
DUBSVNANOGIT <arthur.bic_@hotmail.com>
2024-08-12 17:48:42 -0300

    L4 - Ex2

--- Java/Lista_Faculdade_PS1/Lista de Exercícios 4/Ex2/application/Program.java
+++ Java/Lista_Faculdade_PS1/Lista de Exercícios 4/Ex2/application/Program.java
@@ -1,7 +1,14 @@
 package application;
 
-public class Program {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Program {
+    static int soma(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        System.out.println(soma(a,b));
     }
 }
