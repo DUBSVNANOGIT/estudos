9c17d4baac3de5c671cb01491ddd4acb050585ba
DUBSVNANOGIT <arthur.bic_@hotmail.com>
2024-08-12 18:01:38 -0300

    L4 - Ex3

---
+++ Java/Lista_Faculdade_PS1/Lista de Exercícios 4/Ex3/README.md
@@ -0,0 +1,7 @@
+Desenvolva um programa que receba três notas e seus respectivos pesos a partir do teclado,
+passando-os como argumento para um método que calcule média. A sub-rotina deverá calcular e
+retornar a média ponderada das notas recebidas como parâmetro, considerando os respectivos
+pesos.
+
+
+![img.png](img.png)
--- Java/Lista_Faculdade_PS1/Lista de Exercícios 4/Ex3/application/Program.java
+++ Java/Lista_Faculdade_PS1/Lista de Exercícios 4/Ex3/application/Program.java
@@ -1,7 +1,30 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
+    static double media(double n1, double n2, double n3, int peso1, int peso2, int peso3) {
+        return ((n1 * peso1) + (n2 * peso2) + (n3 * peso3)) / (peso1 + peso2 + peso3);
+    }
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        double n1, n2, n3;
+        int peso1, peso2, peso3;
+        System.out.println("Inform e as três notas: ");
+        System.out.print("Nota 1: ");
+        n1 = sc.nextDouble();
+        System.out.print("Nota 2: ");
+        n2 = sc.nextDouble();
+        System.out.print("Nota 3: ");
+        n3 = sc.nextDouble();
+        System.out.println("Informe os pesos das avaliações: ");
+        System.out.print("Peso 1: ");
+        peso1 = sc.nextInt();
+        System.out.print("Peso 2: ");
+        peso2 = sc.nextInt();
+        System.out.print("Peso 3: ");
+        peso3 = sc.nextInt();
+        System.out.println("A média ponderada é: " + media(n1,n2,n3,peso1,peso2,peso3));
+
     }
 }
