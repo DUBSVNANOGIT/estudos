c809a8c516137cd6a78bc855bd197ff02924b656
DUBSVNANOGIT <arthur.bic_@hotmail.com>
2024-08-13 18:06:15 -0300

    L4 - Ex4

---
+++ Java/Lista_Faculdade_PS1/Lista de Exercícios 4/Ex4/README.md
@@ -0,0 +1,3 @@
+Defina um método em Java chamado CelsiusToFahrenheit, que recebe uma medida de
+temperatura em graus Celsius como parâmetro, calcula e retorna o valor correspondente em graus
+Fahrenheit. Lembre-se que a conversão entre estas medias é dada por: 𝐹 = (9/5) * 𝐶 + 32
--- Java/Lista_Faculdade_PS1/Lista de Exercícios 4/Ex4/application/Program.java
+++ Java/Lista_Faculdade_PS1/Lista de Exercícios 4/Ex4/application/Program.java
@@ -1,7 +1,16 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
+    static double celsiusToFahrenheit(double C) {
+        return (C * (9.0 / 5.0)) + 32;
+    }
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite a temperatura em Celsius: ");
+        double C = sc.nextDouble();
+        double F = celsiusToFahrenheit(C);
+        System.out.println("A temperatura em Fahrenheit é : " + F + "°.");
     }
 }
