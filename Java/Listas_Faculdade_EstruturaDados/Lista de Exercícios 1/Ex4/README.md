5d404352fddc63da07d9bdc9af07df827ed74ef7
DUBSVNANOGIT <arthur.bic_@hotmail.com>
2024-08-14 16:30:25 -0300

    L1 - Ex4

---
+++ Java/Listas_Faculdade_EstruturaDados/Lista de Exercícios 1/Ex4/README.md
@@ -0,0 +1,2 @@
+Exercício 4 - MATRIZ
+Faça um MÉTODO que receba como parâmetro uma matriz de quadrada de números inteiros M (5,5) e retorne a soma dos elementos da diagonal principal desta matriz.
---
+++ Java/Listas_Faculdade_EstruturaDados/Lista de Exercícios 1/Ex4/application/Program.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] mat = new int[5][5];
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+        for (int[] linha : mat) {
+            for (int elemento : linha) {
+                System.out.println(elemento + " ");
+            }
+            System.out.println();
+        }
+        System.out.println(somaDiagonal(mat));
+    }
+    public static int somaDiagonal(int[][] matriz) {
+        int soma = 0;
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j < 5; j++){
+                if (i == j){
+                    soma += matriz[i][j];
+                }
+            }
+        }
+        return soma;
+    }
+}
