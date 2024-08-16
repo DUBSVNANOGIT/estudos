0f66ecf673e80b80f635f4ee5de2d3d659107245
DUBSVNA <141194752+DUBSVNANOGIT@users.noreply.github.com>
2024-08-16 22:32:20 +0000

    Ex5 algo

--- Java/Listas_Faculdade_ProgramaçaoSistemas1/Lista de Exercícios 4/Ex5/application/Program.java
+++ Java/Listas_Faculdade_ProgramaçaoSistemas1/Lista de Exercícios 4/Ex5/application/Program.java
@@ -2,6 +2,29 @@ package application;
 
 public class Program {
     public static void main(String[] args) {
+        tabelaDeTemperatura(-10, 10, 2);
         
+    }
+
+    static void tabelaDeTemperatura (int nMenor, int nMaior, int passo) {}
+
+    static void tabelaDeTemperatura (int nMenor, int nMaior) {
+        while (nMaior >= nMenor) {
+            System.out.printf("%d °C <-> %.2f ° F", nMaior, celsiusToFahrenheit(nMaior));
+            System.out.println();
+            nMaior = nMaior - 5;
+        }
+    }
+
+    @Override
+    static void tabelaDeTemperatura (int nMenor, int nMaior, int passo) {
+        while (nMaior >= nMenor) {
+            System.out.printf("%d °C <-> %.2f ° F", nMaior, celsiusToFahrenheit(nMaior));
+            System.out.println();
+            nMaior = nMaior - passo;
+        }
+    }
+    static double celsiusToFahrenheit(double C) {
+        return (C * (9.0 / 5.0)) + 32;
     }
 }
