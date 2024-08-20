f2e5d5388730e81db5ae37a1800a8fcdaa5970b1
DUBSVNA <141194752+DUBSVNANOGIT@users.noreply.github.com>
2024-08-20 23:54:51 +0000

    L2 - Ex1 update

--- Java/Listas_Faculdade_EstruturaDados/Lista de Exercícios 2/Ex1/application/Program.java
+++ Java/Listas_Faculdade_EstruturaDados/Lista de Exercícios 2/Ex1/application/Program.java
@@ -2,8 +2,9 @@ import java.util.Random;
 
 public class Program {
     public static void main (String[] args){
-        int[][] A = new int [2][2];
-        int[][] B = new int [2][2];
+        //O(n³)
+        int[][] A = new int [800][800];
+        int[][] B = new int [800][800];
         int count = 0;
         //preenche as matrizes A e B com números aleatórios
          preencheA(A);
@@ -12,10 +13,9 @@ public class Program {
          int[][] C = new int [A.length][B[0].length];
         C = multiplica (A,B,count);
         exibeResult(C);
-        System.out.println("Contador: " + count)
         }
 
-        public static void preencheA(int A[][], Random random){
+        public static void preencheA(int A[][]){
             Random random = new Random();
             for (int i = 0; i < A.length; i++) {
                 for (int j = 0; j < A[0].length; j++) {
@@ -23,10 +23,10 @@ public class Program {
                 }
             }
         }
-       
+
         public static void preencheB(int B[][]){
-            Random random =  new Random()
-            for(int i = 0; i < A.length; i++) {
+            Random random =  new Random();
+            for(int i = 0; i < B.length; i++) {
                 for(int j =0; j < B[0].length; j++) {
                     B[i][j] = random.nextInt(10);
                 }
@@ -34,27 +34,27 @@ public class Program {
         }
 
         public static int[][] multiplica (int[][]A, int[][]B, int count) {
-            int count = 0;
             int[][] C = new int [A.length] [B[0].length];
-            for (int i=0; i < A.length; i++)
-                for(int j=0; j. B[0].length; j++) {
+            for (int i=0; i < A.length; i++) {
+                for(int j=0; j < B[0].length; j++) {
                     C[i][j] = 0;
                     for (int k = 0; k < A[0].length; k++) {
                         C[i][j] += A[i][k] * B[k][j];
-                        count++;
+                        count += 1;
                     }
                 }
             }
+            System.out.println("Contador: " + count);
             return C;
         }
 
         public static void exibeResult(int C[][]){
             for (int i = 0; i < C.length; i++) {
-                for (int j = 0; j < C.length; j++) {
-                    System.out.println(C[i][j] + " ");
+                for (int j = 0; j < C[0].length; j++) {
+                    System.out.print(C[i][j] + " ");
                 }
                 System.out.println();
             } 
         }
-    
+
 }
