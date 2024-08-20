d115b6c78d33453b19fa3dcde3a0972e7479ecc6
DUBSVNA <141194752+DUBSVNANOGIT@users.noreply.github.com>
2024-08-20 23:28:55 +0000

    Lista de Exercícios 2 - Ex1

--- Java/Listas_Faculdade_ProgramaçaoSistemas1/Lista de Exercícios 5/Ex2/application/Program.java
+++ Java/Listas_Faculdade_ProgramaçaoSistemas1/Lista de Exercícios 5/Ex2/application/Program.java
@@ -7,7 +7,7 @@ public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        System.out.println("Triangulo")
+        System.out.println("Triângulo")
         System.out.print("Base: ");
         double base = sc.nextDouble();
         System.out.print("Altura: ");
