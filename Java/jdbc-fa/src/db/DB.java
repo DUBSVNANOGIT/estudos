ef038d8237403892dfbb75cde35bc3ffd6a0456e
DUBSVNANOGIT <arthur.bic_@hotmail.com>
2024-08-28 09:44:35 -0300

    DB - getConnection()

--- Java/jdbc-fa/src/db/DB.java
+++ Java/jdbc-fa/src/db/DB.java
@@ -1,11 +1,33 @@
 package db;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class DB {
     private static Connection conn = null;
 
     public Connection getConnection()  {
+        if (conn == null) {
+            try {
+                Properties props;
+                try (FileInputStream fs = new FileInputStream("C:\\Users\\Duqo\\Desktop\\Estudo\\Java\\jdbc-fa\\db.properties")) {
+                    props = new Properties();
+                    props.load(fs);
+                } catch (IOException e) {
+                    throw new DbException(e.getMessage());
+                }
+                String url = props.getProperty("dburl");
+                conn = DriverManager.getConnection(url, props);
 
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+        System.out.println("Conectado ao Banco de Dados");
+        return conn;
     }
 }
