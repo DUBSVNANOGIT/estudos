40b8e8506260f47ea26598857bca0f4af33b7070
DUBSVNA <141194752+DUBSVNANOGIT@users.noreply.github.com>
2024-09-11 01:35:45 +0000

    jdbc done

--- Java/Listas_Faculdade_ProgramaçaoSistemas2/JDBC/gerenciador/src/main/java/jdbc/App.java
+++ Java/Listas_Faculdade_ProgramaçaoSistemas2/JDBC/gerenciador/src/main/java/jdbc/App.java
@@ -1,5 +1,10 @@
 package jdbc;
 
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.*;
+
 /**
  * Hello world!
  */
@@ -11,7 +16,18 @@ public final class App {
      * Says hello to the world.
      * @param args The arguments of the program.
      */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:postgresql://aws-0-sa-east-1.pooler.supabase.com:6543/postgres?user=postgres.xwwanfrmrvlvwrxynxos&password=zowiebowie1*";
+        Connection c = DriverManager.getConnection(url);
+        System.out.println("Conexão bem-sucedidada");
+        String sql = "SELECT * FROM contas";
+        PreparedStatement stm = c.prepareStatement(sql);
+        ResultSet rs = stm.executeQuery();
+        while (rs.next()) {
+            System.out.print(rs.getLong("nro_conta") + " - ");
+            System.out.println(rs.getDouble("saldo"));
+        }
+
+        
     }
 }
