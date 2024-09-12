52446c491eb890b70eada1a0b0120c4f7b94db5c
DUBSVNA <141194752+DUBSVNANOGIT@users.noreply.github.com>
2024-09-12 23:54:29 +0000

    jdbc update

--- Java/Listas_Faculdade_ProgramaçaoSistemas2/JDBC/gerenciador/src/main/java/jdbc/App.java
+++ Java/Listas_Faculdade_ProgramaçaoSistemas2/JDBC/gerenciador/src/main/java/jdbc/App.java
@@ -17,17 +17,41 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) throws SQLException {
-        String url = "jdbc:postgresql://aws-0-sa-east-1.pooler.supabase.com:6543/postgres?user=postgres.xwwanfrmrvlvwrxynxos&password=zowiebowie1*";
-        Connection c = DriverManager.getConnection(url);
-        System.out.println("Conexão bem-sucedidada");
-        String sql = "SELECT * FROM contas";
-        PreparedStatement stm = c.prepareStatement(sql);
-        ResultSet rs = stm.executeQuery();
-        while (rs.next()) {
-            System.out.print(rs.getLong("nro_conta") + " - ");
-            System.out.println(rs.getDouble("saldo"));
-        }
+        String nconta = String.valueOf(123);
+        read();
 
         
+    }
+
+    public static void read() {
+        String url = "jdbc:postgresql://aws-0-sa-east-1.pooler.supabase.com:6543/postgres?user=postgres.xwwanfrmrvlvwrxynxos&password=zowiebowie1*";
+        try {
+        Connection c = DriverManager.getConnection(url);
+        String sql = "SELECT * FROM contas";
+        PreparedStatement statement = c.prepareStatement(sql);
+        ResultSet rs = statement.executeQuery();
+        while (rs.next()) {
+            System.out.println("N°: " + rs.getString("nro_conta") + ", Saldo: " + rs.getString("saldo"));
+        }
+        c.close();
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void read(int n) {
+        String url = "jdbc:postgresql://aws-0-sa-east-1.pooler.supabase.com:6543/postgres?user=postgres.xwwanfrmrvlvwrxynxos&password=zowiebowie1*";
+        String nconta = String.valueOf(n);
+        try {
+        Connection c = DriverManager.getConnection(url);
+        PreparedStatement stmt = c.prepareStatement("SELECT * WHERE nro_conta = " + nconta);
+        ResultSet rs = stmt.executeQuery();
+        while (rs.next()) {
+            System.out.println("N°: " + rs.getString("nro_conta") + ", Saldo: " + rs.getString("saldo"));
+        }
+        c.close();
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
